f48cb4eca6fbfa1334c576fd9e2777dacebe8f74
ganeshsuryawansh <ganeshsuryawanshi594@gmail.com>
2025-03-13 00:01:25 +0530

    ngfor, ngif etc

--- src/app/components/directive/directive.component.ts
+++ src/app/components/directive/directive.component.ts
@@ -1,13 +1,14 @@
-import { NgIf } from '@angular/common';
+import { NgFor, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-directive',
-  imports: [NgIf, FormsModule],
+  imports: [NgIf, FormsModule, NgFor],
   templateUrl: './directive.component.html',
   styleUrl: './directive.component.css'
 })
+
 export class DirectiveComponent {
   isVisible: boolean = true;
 
@@ -15,6 +16,15 @@ export class DirectiveComponent {
   num2 = 0;
 
   selected: string = '';
+
+  cityList: string[] = ['Pune', 'Mumbai', 'Delhi', 'Bangalore', 'Chennai'];
+
+  studentList: any[] = [
+    { name: 'AAA', city: 'Pune', isActive: true },
+    { name: 'BBB', city: 'Pune', isActive: true },
+    { name: 'CCC', city: 'Pune', isActive: false },
+    { name: 'DDD', city: 'Pune', isActive: true },
+  ]
 
   hidediv() {
     this.isVisible = false;
